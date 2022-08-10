c4fb81996292bc4dfe551199e45adf54ed915ef9
Serdar Moran <serdar.moran@gmail.com>
2022-08-10 06:35:26 +0200

    new files added

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString="";
+            inputString = Console.ReadLine();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(inputString);
+
+        }
+    }
+}
